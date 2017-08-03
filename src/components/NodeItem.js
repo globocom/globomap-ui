9b23b14bba6e32a0e88326ecba66c57bc19ee29a
Alan <avanselmo@gmail.com>
2017-08-03 19:33:17 -0300

    NodeItem component

---
+++ src/components/NodeItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './css/NodeItem.css';
+
+
+class NodeItem extends Component {
+  constructor(props) {
+    super(props);
+    this.onItemSelect = this.onItemSelect.bind(this);
+  }
+
+  render() {
+    let node = this.props.node,
+        { name, properties, current } = node;
+
+    return (
+      <div className={"node-item " + (current ? " current" : "")}
+           onClick={this.onItemSelect}>
+        <a href={"#"+ name} className="node-name">{name}</a>
+      </div>
+    );
+  }
+
+  onItemSelect(event) {
+    event.stopPropagation();
+    this.props.setCurrent(this.props.node._id);
+  }
+}
+
+export default NodeItem;
