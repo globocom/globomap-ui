0a7e16ebdb88febde3399cdf8dbf4c9a5a3cfc7b
Alan <alan.vitor@corp.globo.com>
2017-08-28 21:27:16 -0300

    Press enter on search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,12 +5,12 @@ class Search extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
       query: ""
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.onSendSearchQuery = this.onSendSearchQuery.bind(this);
   }
 
@@ -18,19 +18,27 @@ class Search extends Component {
     return (
       <div className="search-box">
         <input className="topcoat-text-input--large" type="search" name="query"
-          value={this.state.query} onChange={this.handleInputChange} />
+          value={this.state.query} onChange={this.handleInputChange} onKeyPress={this.handleKeyPress} />
         <button className="btn-search topcoat-button--large" onClick={this.onSendSearchQuery}>Search</button>
       </div>
     );
   }
 
   handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({[target.name]: value});
+    let target = event.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
   }
 
-  onSendSearchQuery() {
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.onSendSearchQuery(event);
+    }
+  };
+
+  onSendSearchQuery(event) {
+    event.preventDefault();
     this.props.clearStage();
     this.props.clearCurrent();
     this.props.findNodes(this.state.query, this.props.enabledCollections);
