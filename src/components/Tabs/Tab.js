8abebca1674d6648a324e27cdebd38c7c1b3311c
Alan <alan.vitor@corp.globo.com>
2018-01-18 19:14:07 -0200

    Tabs component

---
+++ src/components/Tabs/Tab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Tab = (props) => {
+  return (
+    <li className="tab">
+      <a className={`tab-link ${props.linkClassName} ${props.isActive ? 'active' : ''}`}
+        onClick={(event) => {
+            event.preventDefault();
+            props.onClick(props.tabIndex);
+        }}>
+        <i className={`tab-icon ${props.iconClassName}`}/>
+      </a>
+    </li>
+  );
+}
