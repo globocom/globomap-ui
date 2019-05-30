e4968e751e4d0f0387f7045c695816611c4c841b
Alan <alan.vitor@corp.globo.com>
2019-05-30 20:20:53 -0300

    Add one more CORS config; remove x-powered-by header

--- server/app.js
+++ server/app.js
@@ -33,11 +33,15 @@ https.globalAgent.options.ca = fs.readFileSync(config.certificates);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// remove X-Powered-By header
+app.disable('x-powered-by')
+
 // Add CORS for DEVELOPMENT
 if (config.environment === 'development') {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     next();
   });
 }
