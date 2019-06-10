c7f35fe8da7bce0476880952c26fc06e4032e279
Alan <alan.vitor@corp.globo.com>
2019-06-10 17:57:55 -0300

    Fix and add more AutoMap tests

--- src/components/AutoMap/AutoMap.test.js
+++ src/components/AutoMap/AutoMap.test.js
@@ -7,12 +7,24 @@ import { AutoMap } from './AutoMap';
 chai.use(chaiEnzyme());
 const expect = chai.expect;
 
-describe("AutoMap", function() {
+describe("AutoMap", () => {
+  let component = null;
+
+  beforeAll(() => {
+    const props = { automapNodeList: [] }
+    component = shallow(<AutoMap {...props} />);
+  })
 
   it('should render AutoMap component', () => {
-    const props = { automapNodeList: [] }
-    const component = shallow(<AutoMap {...props} />);
     expect(component).to.exist;
   });
+
+  it('should render at least 1 initial kind button', () => {
+    expect(component.find('.automap-kind .gmap-btn')).to.have.lengthOf(1);
+  });
+
+  it('search input is disabled at initial render', () => {
+    expect(component.find('.automap-search .automap-q')).to.have.attr('disabled', 'disabled');
+  })
 
 });
