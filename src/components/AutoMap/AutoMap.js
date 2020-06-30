965c949bb8f4286c69a5857f6f067301139d03d0
Julio Rama <julio.rama@corp.globo.com>
2020-06-30 12:29:16 -0300

    Added default names to maps created via AutoMap and Advanced Search

--- src/components/AutoMap/AutoMap.js
+++ src/components/AutoMap/AutoMap.js
@@ -19,6 +19,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {
   setStageNodes,
+  setMapName,
   cleanStageNodes } from '../../redux/modules/stage';
 import {
   setCurrentKind,
@@ -172,6 +173,7 @@ export class AutoMap extends React.Component {
     this.props.clearCurrentNode();
     this.props.resetSubNodes();
     this.props.cleanStageNodes();
+    this.props.setMapName(kind.name + ' - ' + node.name);
 
     if (kind.type === 'query') {
       this.props.automapTraversalQuery({
@@ -307,6 +309,7 @@ export default connect(
     automapTraversalQuery,
     automapResetNodes,
     setStageNodes,
+    setMapName,
     cleanStageNodes,
     clearCurrentNode,
     resetSubNodes
